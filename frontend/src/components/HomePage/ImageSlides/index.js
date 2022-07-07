97b691d30b0787bc813b2f8cab9a45d119475386
Paul-Eirik <pmelhus@gmail.com>
2022-07-07 16:38:16 -0400

    added image carousel

--- frontend/src/components/HomePage/ImageSlides/index.js
+++ frontend/src/components/HomePage/ImageSlides/index.js
@@ -1,25 +1,60 @@
 import "./ImageSlides.css";
+import { useEffect, useState } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+// import "./style.css"
+// import images from "./images"
 
 const ImageSlides = () => {
+  const images = [
+    {
+      id: 1,
+      title: "Get in your music zone with ToneZone",
+      class: "image-1-content",
+    },
+    {
+      id: 2,
+      body: "ToneZone is the place to share and discover music with your friends and collaborators.",
+      class: "image-2-content",
+    },
+  ];
+
+  //  const carouselStyle1 = {transform: 'translateX(0%)', transition: 'transform 0.6s ease-in-out 0s', width: '300%', display: "flex", overflow:'hidden', height: '100%'}
+  //  const carouselStyle2 = {transform: 'translateX(-33%)', transition: 'transform 0.6s ease-in-out 0s', width: '300%', display: "flex", overflow:'hidden', height: '100%'}
+
+  // const [carStyle, setCarStyle] = useState(carouselStyle1)
+
+  // const carouselInterval = () => {
+  //   setInterval(carouselCallBack, 1000, carouselStyle1, carouselStyle2)
+  // }
+
+  // const carouselCallBack = (carouselStyle1) => {
+
+  // }
+  // console.log(carouselInterval())
+  // console.log(carStyle)
+
+  const settings = {
+    infinite: true,
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    lazyLoad: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+  };
+
   return (
     <div className="image-container">
-      <div className="image-carousel">
-        <div className="image-1-content">
-          <div className="image-1-title">
-            Get in your music zone with ToneZone
+      <Slider {...settings}>
+        {images.map((item) => (
+          <div className={item.class}>
+            <div className="image-1-title">{item.title}</div>
+            <p className="image-1-tagline">{item.body}</p>
           </div>
-          <p className="image-1-tagline">
-            ToneZone is the place to share and discover music with your friends
-            and collaborators.
-          </p>
-        </div>
-        <div className="image-2">
-          <p className="image-1-tagline">
-            Upload your first track and begin your journey. ToneZone gives you
-            space to create, find your fans, and connect with other artists.
-          </p>
-        </div>
-      </div>
+        ))}
+      </Slider>
     </div>
   );
 };
